aa7ca240a2a26cdd43c3b936939af4903ea86009
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2025-03-21 15:11:18 +0100

    work in progress

---
+++ src/create-table.ts
@@ -0,0 +1,23 @@
+// @license
+// Copyright (c) 2025 Rljson
+//
+// Use of this source code is governed by terms that can be
+// found in the LICENSE file in the root of this package.
+
+import { ColumnSelection } from '@rljson/table';
+
+import { Io } from './io.ts';
+
+/**
+ * Manages creating tables
+ */
+export class CreateTable {
+  constructor(
+    private readonly _io: Io,
+    private readonly _cs: ColumnSelection,
+  ) {}
+
+  // ######################
+  // Private
+  // ######################
+}
