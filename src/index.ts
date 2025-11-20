7f2cf147a71cd03caa39924e64eda1df1db2ecca
Jan <154504062+Jan-Balzer@users.noreply.github.com>
2025-11-20 08:04:48 +0100

    Add conversion functions for rel dbs w/ restrictive naming convention
    * introducing db-name-mapping
    
    * Increase version
    
    * add tests

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@
 // Use of this source code is governed by terms that can be
 // found in the LICENSE file in the root of this package.
 
+export * from './io-db-name-mapping.ts';
 export * from './io-mem.ts';
 export * from './io-test-setup.ts';
 export * from './io-tools.ts';
